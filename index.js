9b455e86b419ffea52dc0562e845d3c7a18e4cb3
Choeles Tung <choelestung@gmail.com>
2018-05-08 21:37:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ function theBeatlesPlay(musician, instrument){
 }
 
 function johnLennonFacts(arrayFacts){
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-  
   var i = 0;
   
   while(i < arrayFacts.length)  {
